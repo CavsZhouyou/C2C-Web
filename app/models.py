0e52428c71198508869c235a2d79f31b1799a843
Blame <qq672804131@gmail.com>
2018-07-03 16:23:05 +0800

    增添了旅游信息和免责声明模型

--- app/models.py
+++ app/models.py
@@ -7,5 +7,25 @@ db = SQLAlchemy()
 class User(db.Model):
     user_id = db.Column(db.Integer,primary_key=True)
 
+#旅行信息推送
+class TravelMessage(db.Model):
+    __tablename__ = 'travelmessage'
+    tmessage_id = db.Column(db.Integer,auto_increment=True,nullable=False,unique=True,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.user_id'))
+    title=db.Column(db.String(100),nullable=False)
+    content = db.Column(db.Text)
+    date = db.Column(db.DateTime,nullable=False,default=datetime.now))
+    addressOftravel=db.Column(db.String(100),nullable=False)
+
+#免责声明
+class Disclaimer(db.Model):
+    __tablename__= 'disclaimer'
+    disclaimer_id = db.Column(db.Integer,auto_increment=True,nullable=False,unique=True,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.user_id'))
+    content = db.Column(db.Text)
+    title = db.Column(db.String(100),nullable=False)
+    publishdate = db.Column(db.Datetime,nullable,default=datetime.now)
+
+
 
 
