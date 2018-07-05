edc645ccaefd0065e6661ce9b88d75563c843b4a
zhouyou <zhouyou.world@outlook.com>
2018-07-05 15:23:11 +0800

    完善前端配置文件，添加elementg-ui和axios等

--- webapp/c2c_system/src/main.js
+++ webapp/c2c_system/src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from './js/axios';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+
+// import utils
+Vue.prototype.$axios = axios;
 
 new Vue({
   router,
---
+++ webapp/c2c_system/vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    configureWebpack: config => {
+        config.entry.mondernize = './src/js/modernizr.js'
+    }
+}
